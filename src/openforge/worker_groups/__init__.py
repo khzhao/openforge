8d216291d183edaa8b1f35a45ffda1d4e49e64ac
Kevin Zhao <kzhao16@gmail.com>
2026-02-25 13:28:42 -0600

    Delete the actor/critic/ref/rollout for now

--- src/openforge/worker_groups/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright 2026 openforge
-
-from .actor_critic_ref_worker_group import ActorCriticRefWorkerGroup
-from .rollout_worker_group import RolloutWorkerGroup
-
-__all__ = ["ActorCriticRefWorkerGroup", "RolloutWorkerGroup"]
